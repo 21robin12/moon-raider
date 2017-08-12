5dafc71b8cbee1ba9065ca48530565b5302fc943
Robin Home PC <robinherd@hotmail.co.uk>
2017-08-12 18:34:43 +0100

    reduce drag

--- src/physics/Constants.ts
+++ src/physics/Constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-    static dragMultiplier: number = 0.06;
+    static dragMultiplier: number = 0.03;
     static spaceShipAcceleration: number = 0.0003;
     static spaceShipRotationVelocity: number = 0.005;
 }
